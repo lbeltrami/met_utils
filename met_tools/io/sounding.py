a4c707555e9a41462baf189853345330868eecda
Leonardo Beltrami <lbeltrami@arpae.it>
2026-05-05 07:52:45 +0000

    changed bufr interpreter function

--- met_tools/io/sounding.py
+++ met_tools/io/sounding.py
@@ -55,34 +55,122 @@ def sond_download_bufr(output_path, start, end, sta=None):
 
 # --------------------------------------------------------------------------------------
 
-def sond_bufr_to_df(file_path):
+def read_bufr_sounding(path):
     """
-    Read radiosonde BUFR data into a pandas DataFrame.
+    Read a BUFR radiosounding file and extract its data and metadata.
+    Converts each BUFR message into one vertical profile with the following structure:
 
-    This function extracts standard meteorological variables from a BUFR file
-    using ECMWF's pdbufr library.
+    - dim:
+        - profile : index identifying each radiosounding (one per BUFR message)
+
+    - coords:
+        - pressure (Pa) : used as the vertical coordinate for all profile variables
+
+    - data_vars (all defined along pressure):
+        - airTemperature (K)
+        - dewpointTemperature (K)
+        - nonCoordinateGeopotentialHeight (m)
+        - windDirection (deg)
+        - windSpeed (m/s)
+
+    - attrs (constant per each profile):
+        - time (year, month, day, hour, minute)
+        - station identifiers (blockNumber, stationNumber)
+        - station geolocation (latitude, longitude)
+        - station elevation (heightOfStationGroundAboveMeanSeaLevel)
 
     Parameters
     ----------
-    file_path : str
-        Path to the BUFR file.
+    path : str
+        Path to the BUFR file containing radiosounding observations.
 
     Returns
     -------
-    pandas.DataFrame
-        DataFrame containing radiosonde profiles with meteorological variables.
+    xr.Dataset
+        A concatenated dataset of all radiosounding profiles in the file.
     """
 
-    import pdbufr
-    import pandas as pd
+    from eccodes import *
+    import numpy as np
+    import xarray as xr
 
-    columns = (
-        "latitude", "longitude", "WMO_station_id", "data_datetime",
-        "pressure", "airTemperature", "dewpointTemperature",
-        "windDirection", "windSpeed"
-    )
+    datasets = []
 
-    df = pdbufr.read_bufr(file_path, columns=columns)
+    # open BUFR file
+    bufr = open(path, 'rb')
 
-    return df
+    # loop for all the messages in the file
+    while 1:
 
+        # get handle for message
+        msg = codes_bufr_new_from_file(bufr)
+        if msg is None:
+            break
+
+        # instruct ecCodes to unpack data
+        codes_set(msg, 'unpack', 1)
+
+        # get sounding site metadata
+        year   = codes_get(msg, "year")
+        month  = codes_get(msg, "month")
+        day    = codes_get(msg, "day")
+        hour   = codes_get(msg, "hour")
+        minute = codes_get(msg, "minute")
+
+        blockNumber   = codes_get(msg, "blockNumber")
+        stationNumber = codes_get(msg, "stationNumber")
+
+        latitude  = codes_get(msg, "latitude")
+        longitude = codes_get(msg, "longitude")
+
+        heightOfStationGroundAboveMeanSeaLevel = codes_get(
+            msg, "heightOfStationGroundAboveMeanSeaLevel"
+        )
+
+        # get vertical profile values
+        pressure = np.array(codes_get_array(msg, "pressure"))
+        nonCoordinateGeopotentialHeight = np.array(
+            codes_get_array(msg, "nonCoordinateGeopotentialHeight")
+        )
+        airTemperature = np.array(codes_get_array(msg, "airTemperature"))
+        dewpointTemperature = np.array(codes_get_array(msg, "dewpointTemperature"))
+        windDirection = np.array(codes_get_array(msg, "windDirection"))
+        windSpeed = np.array(codes_get_array(msg, "windSpeed"))
+
+        # build dataset
+        ds = xr.Dataset(
+            data_vars={
+                "airTemperature": ("pressure", airTemperature),
+                "dewpointTemperature": ("pressure", dewpointTemperature),
+                "nonCoordinateGeopotentialHeight": ("pressure", nonCoordinateGeopotentialHeight),
+                "windDirection": ("pressure", windDirection),
+                "windSpeed": ("pressure", windSpeed),
+            },
+            coords={
+                "pressure": ("pressure", pressure),
+            },
+            attrs={
+                "year": year,
+                "month": month,
+                "day": day,
+                "hour": hour,
+                "minute": minute,
+
+                "blockNumber": blockNumber,
+                "stationNumber": stationNumber,
+
+                "latitude": latitude,
+                "longitude": longitude,
+                "heightOfStationGroundAboveMeanSeaLevel": heightOfStationGroundAboveMeanSeaLevel,
+            }
+        )
+
+        datasets.append(ds)
+
+        # release message handle
+        codes_release(msg)
+
+    # close file
+    bufr.close()
+
+    return xr.concat(datasets, dim="profile")
