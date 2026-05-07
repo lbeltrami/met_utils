cf0e45064cf36a096e862083286fb47802a8fb2f
Leonardo Beltrami <lbeltrami@arpae.it>
2026-05-07 08:18:14 +0000

    added mixing ration func

--- met_tools/compute/thermo.py
+++ met_tools/compute/thermo.py
@@ -22,14 +22,36 @@ def virtual_potential_temperature(p, T, Td):
     # calculate potential temperature
     theta = T * (100000.0 / p) ** 0.286
 
-    # calculate water pressure using Magnus-Tetens formula
+    # calculate mixing ratio
+    q = mixing_ratio_from_dewpoint(p, Td)
+
+    return theta * (1 + 0.61 * q)
+
+# ----------------------------------------------------------------------------
+
+def mixing_ratio_from_dewpoint(p, Td):
+    """
+    Compute water vapor mixing ratio from pressure and dew point temperature.
+
+    Parameters
+    ----------
+    p : array-like
+        Pressure (Pa)
+    Td : array-like
+        Dew point temperature (K)
+
+    Returns
+    -------
+    array-like
+        Mixing ratio (kg/kg)
+    """
+
+    import numpy as np
+
+    # calculate water pressure using Magnus-Tetens formula (hPa)
     e = 6.1094 * np.exp((17.625 * (Td - 273.15)) / ((Td - 273.15) + 243.04))
-    
-    # convert from Pa to hPa
+
+    # convert from hPa to Pa
     e = e * 100.0
 
-    # calculate mixing ratio
-    q = 0.622 * e / (p - e)
-
-    # calculate virtual potential temperature
-    return theta * (1 + 0.61 * q)
+    return 0.622 * e / (p - e)
